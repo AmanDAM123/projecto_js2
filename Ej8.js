6fca9845298f7fc6f4f0eae007817d90571647e2
AmanDAM123 <aullbar296@g.educaand.es>
2025-10-23 13:18:51 +0200

    Ej8

---
+++ Ej8.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')();
+
+let numero = prompt("Introduce un número entero:");
+
+if (numero == 0) {
+    console.log("El número no es par ni impar");
+} else if (numero % 2 == 0) {
+    console.log("El número es par");
+} else {
+    console.log("El número es impar");
+}
